fca049bc05ca02fabffa1607e174df6db9ec8b70
GitSA-2025 <accesssystemfatec.com>
2025-08-23 14:57:07 -0300

    8º commit

--- models/accessRegister.js
+++ models/accessRegister.js
@@ -1,15 +1,44 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('./index');
+const sequelize = require('./index'); 
 
 const AccessRegister = sequelize.define('AccessRegister', {
-    idRegister: {type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true},
-    nome: {type: DataTypes.STRING, allowNull: false },
-    cpf: {type: DataTypes.STRING, allowNull: false },
-    tipo: {type: DataTypes.ENUM('colaborador', 'visitante'), allowNull: false },
-    data: {type: DataTypes.DATE, allowNull: false},
-    hrentrada: {type: DataTypes.TIME, allowNull: false},
-    hrsaida: {type: DataTypes.TIME, allowNull: false},
-    placa: {type: DataTypes.STRING, allowNull: true}
+    idRegister: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cpf: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    tipo: {
+        type: DataTypes.ENUM('colaborador', 'visitante'),
+        allowNull: false
+    },
+    data: {
+        type: DataTypes.DATEONLY, 
+        allowNull: false
+    },
+    hrentrada: {
+        type: DataTypes.TIME, 
+        allowNull: false
+    },
+    hrsaida: {
+        type: DataTypes.TIME,
+        allowNull: false
+    },
+    placa: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    freezeTableName: true, 
+    timestamps: false      
 });
 
 module.exports = AccessRegister;
--- models/deliveryRegister.js
+++ models/deliveryRegister.js
@@ -1,13 +1,36 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('./index');
+const sequelize = require('./index');
 
 const DeliveryRegister = sequelize.define('DeliveryRegister', {
-    idRegister: {type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true},
-    nome: {type: DataTypes.STRING, allowNull: false },
-    telefone: {type: DataTypes.STRING, allowNull: false},
-    data: {type: DataTypes.DATE, allowNull: false},
-    hrentrada: {type: DataTypes.TIME, allowNull: false},
-    placa: {type: DataTypes.STRING, allowNull: true},
+    idRegister: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    telefone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    data: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    hrentrada: {
+        type: DataTypes.TIME,
+        allowNull: false
+    },
+    placa: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    freezeTableName: true,
+    timestamps: false
 });
 
 module.exports = DeliveryRegister;
