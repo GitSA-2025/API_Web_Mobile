1e32c40dc19a3baed371240e30897aadab073b2c
GitSA-2025 <accesssystemfatec@gmail.com>
2025-08-25 18:27:21 -0300

    Update appController.js

--- controllers/appController.js
+++ controllers/appController.js
@@ -3,33 +3,33 @@ require('dotenv').config();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const UserAPP = require('../models/userApp');
-const DeliveryRegister = require('../models/deliveryRegister');
+const { DeliveryRegister } = require('../models');
 const AccessRegister = require('../models/accessRegister');
 const { generate2FACode } = require('../utils/generate2FACode');
 const { send2FACode, transporter } = require('../services/mailService');
 
 
 async function cadastrarAPP(req, res) {
-    try {
-        const { nome, email, telefone, senha } = req.body;
-        const senhaHash = await bcrypt.hash(senha, 10);
-        const codigo2FA = generate2FACode();
+  try {
+    const { nome, email, telefone, senha } = req.body;
+    const senhaHash = await bcrypt.hash(senha, 10);
+    const codigo2FA = generate2FACode();
 
-        const user = await UserAPP.create({
-            nome,
-            email,
-            telefone,
-            senha: senhaHash,
-            codigo2FA
-        });
+    const user = await UserAPP.create({
+      nome,
+      email,
+      telefone,
+      senha: senhaHash,
+      codigo2FA
+    });
 
-        await send2FACode(email, codigo2FA);
-        res.status(201).json({ message: 'Usuário cadastrado! Verifique o código enviado por e-mail.' });
-    }
-    catch (err) {
-        console.error('Erro no cadastro:', err);
-        res.status(500).json({ error: 'Erro ao cadastrar usuário. Detalhes no terminal.' });
-    }
+    await send2FACode(email, codigo2FA);
+    res.status(201).json({ message: 'Usuário cadastrado! Verifique o código enviado por e-mail.' });
+  }
+  catch (err) {
+    console.error('Erro no cadastro:', err);
+    res.status(500).json({ error: 'Erro ao cadastrar usuário. Detalhes no terminal.' });
+  }
 }
 
 async function verificar2FAAPP(req, res) {
@@ -44,7 +44,7 @@ async function verificar2FAAPP(req, res) {
 
 async function loginAPP(req, res) {
   const { email, senha } = req.body;
-  const user = await User.findOne({ where: { email } });
+  const user = await UserAPP.findOne({ where: { email } });
   if (!user || !(await bcrypt.compare(senha, user.senha))) return res.status(401).json({ error: 'Credenciais inválidas' });
   if (!user.verificado2FA) return res.status(403).json({ error: '2FA não verificado' });
 
@@ -53,10 +53,10 @@ async function loginAPP(req, res) {
 }
 
 async function editarContaAPP(req, res) {
-    const { nome, email, telefone } = req.body;
-    try {
-        const user = await UserAPP.findByPk(req.userId);
-        if (!user) return res.status(404).json({ error: "Usuário não encontrado." });
+  const { nome, email, telefone } = req.body;
+  try {
+    const user = await UserAPP.findByPk(req.userId);
+    if (!user) return res.status(404).json({ error: "Usuário não encontrado." });
     user.nome = nome;
     user.telefone = telefone.replace(/\D/g, '');
     user.email = email;
@@ -68,8 +68,12 @@ async function editarContaAPP(req, res) {
 }
 
 async function criarRegistroEntrega(req, res) {
-  try{
-    const { nome, data, telefone, hrentrada, placa } = req.body;
+  try {
+    const { nome, telefone, placa } = req.body;
+
+    const agora = new Date();
+    const data = agora.toISOString().split('T')[0];
+    const hrentrada = agora.toTimeString().split(' ')[0];
 
     const deliveryRegister = await DeliveryRegister.create({
       nome,
@@ -78,40 +82,36 @@ async function criarRegistroEntrega(req, res) {
       hrentrada,
       placa
     });
-  }
-  catch (err) {
+
+   
+    res.status(201).json({ message: 'Entrega registrada com sucesso.', deliveryRegister });
+  } catch (err) {
     console.error('Erro no registro de entrega:', err);
-    res.status(500).json({ error: 'Erro ao registrar a entrega. Detalhes no terminal.'});
+    res.status(500).json({ error: 'Erro ao registrar a entrega. Detalhes no terminal.' });
   }
 }
 
-async function criarRegistroEntrada(req, res){
-  try{
+
+async function criarRegistroEntrada(req, res) {
+  try {
     const { nome, tipo, cpf, placa } = req.body;
 
-    if(!nome || !tipo || !cpf){
-      return res.status(400).json({ error: 'Nome, tipo de pessoa e CPF estão em branco. Preencha os campos corretamente.'});
+    if (!nome || !tipo || !cpf) {
+      return res.status(400).json({ error: 'Nome, tipo de pessoa e CPF estão em branco. Preencha os campos corretamente.' });
     }
 
-    const verifPlaca = "";
-
-    if(placa == "" || !placa){
-      verifPlaca = "Não se aplica.";
-    }
-    else {
-      verifPlaca = placa;
-    }
+    const verifPlaca = placa && placa.trim() !== '' ? placa.trim() : 'Não se aplica.';
 
     const cpfRegex = /^\d{11}$/;
-    if (!cpfRegex.test(cpf)){
-      return res.status(400).json({ error: 'Formato do CPF inválido. Use apenas 11 dígitos numéricos.'});
+    if (!cpfRegex.test(cpf)) {
+      return res.status(400).json({ error: 'Formato do CPF inválido. Use apenas 11 dígitos numéricos.' });
     }
 
     const cpfHast = await bcrypt.hash(cpf, 10);
 
     const agora = new Date();
     const data = agora.toISOString().split('T')[0];
-    const hrentrada = agora.toTimeString().split('')[0];
+    const hrentrada = agora.toTimeString().split(' ')[0];
 
     const registro = await AccessRegister.create({
       nome: nome,
@@ -123,51 +123,116 @@ async function criarRegistroEntrada(req, res){
       placa: verifPlaca,
     });
 
-    res.status(201).json({ msg: 'Entrada registrada com sucesso!', registro: {
-      id: registro.id,
-      nome: registro.nome,
-      tipo: registro.tipo,
-      data: registro.data,
-      placa: registro.placa,
-    }})
+    res.status(201).json({
+      msg: 'Entrada registrada com sucesso!', registro: {
+        id: registro.idRegister,
+        nome: registro.nome,
+        tipo: registro.tipo,
+        data: registro.data,
+        placa: registro.placa,
+      }
+    })
   }
-  catch(err){
+  catch (err) {
     console.error('Erro ao registro entrada: ', err)
-    res.status(500).json({error: 'Erro ao registrar entrada.'});
+    res.status(500).json({ error: 'Erro ao registrar entrada.' });
   }
 }
 
 async function exbirRegistrosEntrega(req, res) {
-  try{
+  try {
     const entregas = await DeliveryRegister.findAll({
-      order: [['data', 'DESC'], ['hrentrada', 'DESC']]
+      order: [['data', 'ASC'], ['hrentrada', 'ASC']]
     });
 
     res.status(200).json(entregas);
   }
-  catch(err){
+  catch (err) {
     console.error('Erro ao listar entregas:', err);
-    res.status(500).json({error: 'Erro ao listar entregas.'});
+    res.status(500).json({ error: 'Erro ao listar entregas.' });
   }
 }
 
 async function exbirRegistrosEntrada(req, res) {
-  try{
+  try {
     const entregas = await AccessRegister.findAll({
       order: [['data', 'DESC'], ['hrentrada', 'DESC']]
     });
 
     res.status(200).json(entregas);
   }
-  catch(err){
+  catch (err) {
     console.error('Erro ao listar entregas:', err);
-    res.status(500).json({error: 'Erro ao listar entregas.'});
+    res.status(500).json({ error: 'Erro ao listar entregas.' });
   }
 }
 
 async function verContaAPP(req, res) {
-  const user = await User.findByPk(req.userId);
-  res.json(user);
+  try {
+    const user = await UserAPP.findByPk(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro interno' });
+  }
+}
+
+async function editarRegistroEntrada(req, res) {
+  const { nome, tipo, cpf, placa } = req.body;
+  const idRegister = req.params.idRegister;
+  try {
+    const registro = await AccessRegister.findByPk(idRegister);
+    if (!registro) return res.status(404).json({ error: "Registro de entrada não encontrado." });
+
+    const cpfRegex = /^\d{11}$/;
+    if (!cpfRegex.test(cpf)) {
+      return res.status(400).json({ error: 'Formato do CPF inválido. Use apenas 11 dígitos numéricos.' });
+    }
+
+    const cpfHast = await bcrypt.hash(cpf, 10);
+
+    const verifPlaca = placa && placa.trim() !== '' ? placa.trim() : 'Não se aplica.';
+
+    const agora = new Date();
+    const hrsaida = agora.toTimeString().split(' ')[0];
+
+    registro.nome = nome;
+    registro.tipo = tipo;
+    registro.cpf = cpfHast;
+    registro.placa = verifPlaca;
+    registro.hrsaida = hrsaida;
+    await registro.save();
+    res.json({ message: "Registro de entrada atualizado com sucesso." });
+  }
+  catch(err){
+    res.status(500).json({ error: "Erro ao atualizar o registro." });
+    console.log(err);
+  }
+}
+
+async function editarRegistroEntrega(req, res) {
+  const { nome, telefone, placa } = req.body;
+  const idRegister = req.params.idRegister;
+  try {
+    const registro = await DeliveryRegister.findByPk(idRegister);
+    if (!registro) return res.status(404).json({ error: "Registro de entrada não encontrado." });
+
+
+    registro.nome = nome;
+    registro.telefone = telefone;
+    registro.placa = placa;
+    await registro.save();
+    res.json({ message: "Registro de entrada atualizado com sucesso." });
+  }
+  catch(err){
+    res.status(500).json({ error: "Erro ao atualizar o registro." });
+    console.log(err);
+  }
 }
 
 module.exports = {
@@ -180,4 +245,6 @@ module.exports = {
   exbirRegistrosEntrega,
   exbirRegistrosEntrada,
   verContaAPP,
+  editarRegistroEntrada,
+  editarRegistroEntrega
 };
