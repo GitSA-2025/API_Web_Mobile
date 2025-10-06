225896b4ce0888f510c38a36a37723e8df08b01f
GitSA-2025 <accesssystemfatec@gmail.com>
2025-10-06 00:20:14 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,9 @@
 const postgres = require('postgres');
 
 const connectionString = process.env.DATABASE_URL;
-const sql = postgres(connectionString);
+const sql = postgres(connectionString, {
+  max: 10,       
+  ssl: 'require',
+});
 
 module.exports = sql;
