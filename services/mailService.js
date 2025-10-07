da95581747320a13e3fc224528e1ff114eb0ef55
GitSA-2025 <accesssystemfatec@gmail.com>
2025-10-07 10:04:22 -0300

    Update mailService.js

--- services/mailService.js
+++ services/mailService.js
@@ -2,11 +2,14 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 20000,
 });
 
 async function send2FACode(email, code) {
