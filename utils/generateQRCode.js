6da365bc3ee279e0dec44b8ae72366dea45b90e9
GitSA-2025 <accesssystemfatec.com>
2025-11-22 18:28:49 -0300

    .

--- utils/generateQRCode.js
+++ utils/generateQRCode.js
@@ -3,11 +3,16 @@ import { v4 as uuidv4 } from "uuid";
 
 export async function generateQRCode(data) {
   const json = JSON.stringify(data);
-  return await QRCode.toDataURL(json);
+  return await QRCode.toDataURL(json, {
+    type: "image/png",
+    width: 300,
+    margin: 1,
+  });
 }
+
 export async function generateQRCodeAsFile(data, filename = null) {
   const json = JSON.stringify(data);
-  const id = filename || uuidv4();
+  const id = filename || crypto.randomUUID();
 
   const qrBase64 = await QRCode.toDataURL(json);
 
