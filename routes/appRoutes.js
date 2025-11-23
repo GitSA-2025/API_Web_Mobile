66dcf175a2a9c46dd48b28818351ecb1ca888c24
GitSA-2025 <accesssystemfatec.com>
2025-11-23 20:15:14 -0300

    .

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -87,6 +87,6 @@ router.post("/app/aprovQrCode/:id_request", authMiddleware, async (c) => {
 router.get("/app/verSolic", authMiddleware, async (c) => await verSolicitacoes(c));
 
 router.post("/app/editarPerfil", authMiddleware, async (c) => await editarPerfil(c));
-router.post("/conta", authMiddleware, async (c) => await verConta(c));
+router.post("/app/conta", authMiddleware, async (c) => await verConta(c));
 
 export default router;
